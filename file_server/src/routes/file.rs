f93a68c35af5b59b641cec47cc11712c545678bf
Grant Gryczan <grantgryczan@gmail.com>
2024-01-31 03:51:16 -0500

    Improve route handler error messages

--- file_server/src/routes/file.rs
+++ file_server/src/routes/file.rs
@@ -1,8 +1,10 @@
 //! Route handlers for routes to files.
 
 use axum::response::IntoResponse;
+use axum_macros::debug_handler;
 
 /// Route handler for `GET` on routes to files.
+#[debug_handler]
 pub(crate) async fn get() -> impl IntoResponse {
     "TODO"
 }
--- file_server/src/routes/root.rs
+++ file_server/src/routes/root.rs
@@ -1,10 +1,12 @@
 //! Route handlers for `/`.
 
 use axum::response::{IntoResponse, Redirect};
+use axum_macros::debug_handler;
 
 use crate::WEBSITE_URI;
 
 /// Route handler for `GET /`.
+#[debug_handler]
 pub(crate) async fn get() -> impl IntoResponse {
     Redirect::permanent(WEBSITE_URI)
 }
