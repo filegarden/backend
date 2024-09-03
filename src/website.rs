f8cf74ca6c9ac7c47657bd89876a8abdd6432aa9
Grant Gryczan <grantgryczan@gmail.com>
2024-09-03 03:55:17 -0400

    Only serve the website to `GET` requests

--- src/website.rs
+++ src/website.rs
@@ -2,12 +2,17 @@
 
 use axum::{
     extract::Request,
+    http::{Method, StatusCode},
     response::{IntoResponse, Response},
 };
 
 /// The service function to handle incoming requests for the HTTP API.
 pub(super) fn handle(request: Request) -> Response {
-    drop(request);
+    let (request, _body) = request.into_parts();
+
+    if request.method != Method::GET {
+        return StatusCode::METHOD_NOT_ALLOWED.into_response();
+    }
 
     "TODO".into_response()
 }
