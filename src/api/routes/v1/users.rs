695087e024d25abbb3e877dbf39d8d656655fae2
Grant Gryczan <grantgryczan@gmail.com>
2024-09-04 03:52:17 -0400

    Reorganize API handler

--- src/api/routes/v1/users.rs
+++ src/api/routes/v1/users.rs
@@ -20,6 +20,26 @@ use crate::{
     id::Id,
 };
 
+/// The type to create new user IDs with.
+type NewUserId = Id<[u8; 8]>;
+
+/// Hashes and salts a password using Argon2.
+///
+/// # Errors
+///
+/// Fails if the CSPRNG fails when generating salt.
+fn hash_password(password: &str) -> Result<String, rand::Error> {
+    let mut salt = [0; Salt::RECOMMENDED_LENGTH];
+    rand::thread_rng().try_fill_bytes(&mut salt)?;
+
+    let salt_string = SaltString::encode_b64(&salt).expect("salt should be valid");
+
+    Ok(Argon2::default()
+        .hash_password(password.as_bytes(), &salt_string)
+        .expect("password hashing should be infallible")
+        .to_string())
+}
+
 /// A `POST` request body for this API route.
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase", deny_unknown_fields)]
@@ -38,17 +58,6 @@ pub struct PostRequest {
     pub password: UserPassword,
 }
 
-/// The type to create new user IDs with.
-type NewUserId = Id<[u8; 8]>;
-
-/// A `POST` response body for this API route.
-#[derive(Debug, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct PostResponse {
-    /// The user's ID.
-    pub id: NewUserId,
-}
-
 /// Creates a new user and sends them an account verification email.
 ///
 /// # Errors
@@ -58,17 +67,7 @@ pub struct PostResponse {
 pub async fn post(Json(body): Json<PostRequest>) -> Response<PostResponse> {
     let user_id = NewUserId::generate()?;
 
-    let salt_string = SaltString::encode_b64(&{
-        let mut salt = [0_u8; Salt::RECOMMENDED_LENGTH];
-        rand::thread_rng().try_fill_bytes(&mut salt)?;
-        salt
-    })
-    .expect("salt should be valid");
-
-    let password_hash = Argon2::default()
-        .hash_password(body.password.as_bytes(), &salt_string)
-        .expect("password hashing should be infallible")
-        .to_string();
+    let password_hash = hash_password(&body.password)?;
 
     sqlx::query!(
         "INSERT INTO users (id, email, name, birthdate, password_hash) VALUES ($1, $2, $3, $4, $5)",
@@ -82,4 +81,12 @@ pub async fn post(Json(body): Json<PostRequest>) -> Response<PostResponse> {
     .await?;
 
     Ok((StatusCode::CREATED, Json(PostResponse { id: user_id })))
+}
+
+/// A `POST` response body for this API route.
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PostResponse {
+    /// The user's ID.
+    pub id: NewUserId,
 }
