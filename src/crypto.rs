0d746b588f339ddef95e85d2d7879a5cd80b0bf9
Grant Gryczan <grantgryczan@gmail.com>
2024-12-05 23:55:15 -0500

    Exclude `O` from short codes

--- src/crypto.rs
+++ src/crypto.rs
@@ -51,9 +51,11 @@ pub(crate) fn verify_hash<T: AsRef<[u8]>>(bytes: &T, hash_phc_format: &str) -> b
 }
 
 /// All the characters can be in a string outputted by [`generate_short_code`].
-const SHORT_CODE_CHARS: [char; 36] = [
+///
+/// `O` is excluded because it's often mistaken for `0`.
+const SHORT_CODE_CHARS: [char; 35] = [
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-    'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 ];
 
 /// The length of a string outputted by [`generate_short_code`].
